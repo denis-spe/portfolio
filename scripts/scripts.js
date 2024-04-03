13e5b5a3916a0245ca5d3796fcc73bfaa4dc2d34
denis-spe <denisbrian07@gmail.com>
2024-04-03 02:06:59 +0200

    Fixed the cannot read of properties of null error

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,6 @@
 class Dataclass {
   // Get the open and close button
-  static openMenuBtn = document.querySelector("#open-menu-btn");
-
-  // Fetch all spans in open-menu-btn id
-  static openMenuBtnSpans = document.querySelectorAll("#open-menu-btn span");
-
-  static closeMenuBtn = document.querySelector("#close-menu-btn");
-
-  // Fetch all spans in close-menu-btn id
-  static closeMenuBtnSpans = document.querySelectorAll("#close-menu-btn span");
+  static menuBtn = document.querySelector("#menu-btn");
 
   // Get the aside tag
   static aside = document.querySelector("aside");
@@ -32,8 +24,7 @@ class App {
     var context = this;
 
     document.addEventListener("DOMContentLoaded", function() {
-      context.openMenuBtn()
-      context.closeMenuBtn()
+      context.MenuBtn()
     })
   }
 
@@ -46,9 +37,9 @@ class App {
     // Hide the main title
     Dataclass.mainTitle.style.display = "none";
     
-    Dataclass.openMenuBtn.classList.remove("show-menu-btn");
+    Dataclass.menuBtn.classList.remove("show-menu-btn");
     setTimeout(() => {
-      Dataclass.openMenuBtn.classList.add("close-menu-btn");
+      Dataclass.menuBtn.classList.add("close-menu-btn");
       Dataclass.asideContents.style.display = 'flex'
     }, 100);
 
@@ -71,8 +62,8 @@ class App {
     `
     Dataclass.mainTitle.style.display = "block";
     
-    Dataclass.openMenuBtn.classList.remove("close-menu-btn");
-    Dataclass.openMenuBtn.classList.add("show-menu-btn");
+    Dataclass.menuBtn.classList.remove("close-menu-btn");
+    Dataclass.menuBtn.classList.add("show-menu-btn");
     Dataclass.asideContents.style.display = 'none'
 
     // Remove open aside class from aside tag.
@@ -89,68 +80,16 @@ class App {
   /**
    * Handle open menu button
    */
-  openMenuBtn() {
+  MenuBtn() {
+    Dataclass.menuBtn.addEventListener("click", () => {
 
-    Dataclass.openMenuBtn.addEventListener("click", () => {
-
-      /*setTimeout(() => {
-        navigator.vibrate(0); // Stop the vibration after 3 seconds
-      }, 100);*/
-      /*
-
-      // Remove the close aside class from aside tag.
-      Dataclass.aside.classList.remove('close-aside')
-      // Then add open aside class to aside tag.
-      Dataclass.aside.classList.add('open-aside');
-
-      // Remove hide close aside menu button
-      // from close menu button in aside tag
-      Dataclass.closeMenuBtn.classList.remove('hide-close-aside-menu-btn')
-
-      setTimeout(() => {
-        // Show close menu button after the 
-        // opening aside
-        Dataclass.closeMenuBtn.classList.add('show-close-aside-menu-btn');
-      }, 100)
-
-      // Disable the scroll on body.
-      document.body.style.overflow = "hidden"
-      
-      navigator.vibrate(50);
-      */
-
-      const clicked = Dataclass.openMenuBtn.classList.toggle('c');
+      const clicked = Dataclass.menuBtn.classList.toggle('c');
 
       if (clicked) {
         this.openAsideMenu()
       } else {
         this.closeAsideMenu()
       }
-
-    })
-  }
-
-  /**
-   * Handle close menu button
-   */
-  closeMenuBtn() {
-    Dataclass.closeMenuBtn.addEventListener("click", () => {
-      // Remove open aside class from aside tag.
-      Dataclass.aside.classList.remove('open-aside')
-
-      // Then add close aside class to aside tag.
-      Dataclass.aside.classList.add('close-aside');
-
-      // Remove show close aside menu button
-      // from close menu button in aside tag
-      Dataclass.closeMenuBtn.classList.remove('show-close-aside-menu-btn')
-
-      // Add hide close menu button when the 
-      // closing aside
-      Dataclass.closeMenuBtn.classList.add('hide-close-aside-menu-btn')
-
-      // Enable the scroll on body
-      document.body.style.overflow = "auto"
 
     })
   }
