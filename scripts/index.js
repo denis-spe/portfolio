ba5e94d8772b8db044afdeb215837502d9c35095
denis-spe <denisbrian07@gmail.com>
2025-07-12 03:03:19 +0200

    Added animate on start up page and changed the loading screen to green.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { App } from './app.js'
+import {App} from './app.js'
 
 
 // Calling the App object
@@ -6,10 +6,17 @@ new App();
 
 window.addEventListener("load", () => {
     const loading = document.querySelector("#loading")
+    const headerTitleContainer = document.querySelector(".header-title-container")
+    const menuBtnContainer = document.querySelector(".menu-btn-container")
+    const contactBrand = document.querySelector(".contact-brand")
 
     loading.classList.add(".fade-out")
 
     setTimeout(() => {
         loading.style.display = "none"
     }, 600)
+
+    headerTitleContainer.classList.add("animate-title-on-start")
+    menuBtnContainer.classList.add("animate-menu-btn-on-start")
+    contactBrand.classList.add("animate-contact-brand-on-start")
 })
