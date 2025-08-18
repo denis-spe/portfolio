2924f75746613b3da3745185e461efa81f8a30b6
denis-spe <denisbrian07@gmail.com>
2025-08-18 02:23:43 +0200

    Added icons on aside navigation

--- scripts/app.js
+++ scripts/app.js
@@ -1,232 +1,232 @@
-import { Dataclass } from './dataclass.js'
-import { intersection, arrowIntersection } from './intersection.js'
-import { dataPreprocessing } from './handleData.js'
+import {Dataclass} from './dataclass.js'
+import {arrowIntersection, intersection} from './intersection.js'
+
+import {dataPreprocessing} from './handleData.js'
 
 export class App {
-  // Constructor
-  constructor() {
-    const context = this;
-    
-    document.addEventListener("DOMContentLoaded", function() {
-      context.menuBtn();
-      context.menuItems();
-      context.background();
-      context.fields();
-      dataPreprocessing();
-      arrowIntersection();
-      intersection();
-    });
-  }
-  
-  
-  
-  /**
-   * Animate the list element
-   */
-  animateLi(element, delay, dur) {
-    element.style = `
+    // Constructor
+    constructor() {
+        const context = this;
+
+        document.addEventListener("DOMContentLoaded", function () {
+            context.menuBtn();
+            context.menuItems();
+            context.background();
+            context.fields();
+            arrowIntersection();
+            intersection();
+            dataPreprocessing();
+        });
+    }
+
+
+    /**
+     * Animate the list element
+     */
+    animateLi(element, delay, dur) {
+        element.style = `
   animation-name: animate-li-element;
   animation-timing-function: ease;
   animation-duration: ${dur}ms;
   animation-fill-mode: forwards;
   animation-delay: ${delay}ms;
   `;
-  }
-  
-  openAsideMenu() {
-    Dataclass.header.style = `
+    }
+
+    openAsideMenu() {
+        Dataclass.header.style = `
     justify-content: end;
     background: none;
     `;
-    const li = document.querySelectorAll("aside li");
-    li.forEach((item, index) => {
-      if (index == 0) this.animateLi(item, (index + 0.2) * 100, index * 20);
-      else this.animateLi(item, index * 100, index * 20);
-    });
-    
-    // Hide the main title
-    Dataclass.mainTitle.style.display = "none";
-    
-    Dataclass.menuBtn.classList.remove("show-menu-btn");
-    setTimeout(() => {
-      Dataclass.menuBtn.classList.add("close-menu-btn");
-      Dataclass.asideContents.style.display = "flex";
-      Dataclass.praise.style.display = "flex";
-    }, 100);
-    
-    // Remove the close aside class from aside tag.
-    Dataclass.aside.classList.remove("close-aside");
-    
-    // Then add open aside class to aside tag.
-    Dataclass.aside.classList.add("open-aside");
-    
-    // Disable the scroll on body.
-    document.body.style.overflow = "hidden";
-    
-    navigator.vibrate(50);
-  }
-  
-  closeAsideMenu() {
-    Dataclass.header.style = `
+        const li = document.querySelectorAll("aside li");
+        li.forEach((item, index) => {
+            if (index == 0) this.animateLi(item, (index + 0.2) * 100, index * 20);
+            else this.animateLi(item, index * 100, index * 20);
+        });
+
+        // Hide the main title
+        Dataclass.mainTitle.style.display = "none";
+
+        Dataclass.menuBtn.classList.remove("show-menu-btn");
+        setTimeout(() => {
+            Dataclass.menuBtn.classList.add("close-menu-btn");
+            Dataclass.asideContents.style.display = "flex";
+            Dataclass.praise.style.display = "flex";
+        }, 100);
+
+        // Remove the close aside class from aside tag.
+        Dataclass.aside.classList.remove("close-aside");
+
+        // Then add open aside class to aside tag.
+        Dataclass.aside.classList.add("open-aside");
+
+        // Disable the scroll on body.
+        document.body.style.overflow = "hidden";
+
+        navigator.vibrate(50);
+    }
+
+    closeAsideMenu() {
+        Dataclass.header.style = `
     justify-content: space-between;
     background: var(--header-bg);
     backdrop-filter: var(--header-blur);
     `;
-    Dataclass.mainTitle.style.display = "flex";
-    
-    Dataclass.menuBtn.classList.remove("close-menu-btn");
-    Dataclass.menuBtn.classList.add("show-menu-btn");
-    Dataclass.asideContents.style.display = "none";
-    Dataclass.praise.style.display = "none";
-    
-    // Remove open aside class from aside tag.
-    Dataclass.aside.classList.remove("open-aside");
-    
-    // Then add close aside class to aside tag.
-    Dataclass.aside.classList.add("close-aside");
-    
-    // Enable the scroll on body
-    document.body.style.overflow = "auto";
-  }
-  
-  // Methods
-  /**
-   * Handle open menu button
-   */
-  menuBtn() {
-    Dataclass.menuBtn.addEventListener("click", () => {
-      const clicked = Dataclass.menuBtn.classList.toggle("c");
-      if (clicked) {
-        this.openAsideMenu();
-      } else {
-        this.closeAsideMenu();
-      }
-    });
-  }
-  
-  menuItems() {
-    const lists = document.querySelectorAll(".aside-contents ul li p")
-    
-    lists.forEach(li => {
-      li.addEventListener("click", () => {
-        this.menuItem(li.dataset.sectionId)
-      })
-    })
-  }
-  
-  menuItem(sectionId) {
-    const clicked = Dataclass.menuBtn.classList.toggle("c");
-    if (clicked) {
-      this.openAsideMenu();
-    } else {
-      this.closeAsideMenu();
-      this.menuItmHyperlink(sectionId)
+        Dataclass.mainTitle.style.display = "flex";
+
+        Dataclass.menuBtn.classList.remove("close-menu-btn");
+        Dataclass.menuBtn.classList.add("show-menu-btn");
+        Dataclass.asideContents.style.display = "none";
+        Dataclass.praise.style.display = "none";
+
+        // Remove open aside class from aside tag.
+        Dataclass.aside.classList.remove("open-aside");
+
+        // Then add close aside class to aside tag.
+        Dataclass.aside.classList.add("close-aside");
+
+        // Enable the scroll on body
+        document.body.style.overflow = "auto";
     }
-  }
-  
-  menuItmHyperlink(sectionId) {
-    document.querySelector(sectionId).scrollIntoView({
-      behavior: 'smooth'
-    })
-  }
-  
-  
-  
-  background() {
-    for (let i = 0; i < 180; i++) {
-      // Create the div container with square class
-      const div = document.createElement("div");
-      div.classList.add("square");
-      div.classList.add("square-" + i);
-      
-      // Append the div element to design container
-      Dataclass.designContainer.appendChild(div);
-    }
-  }
-  
-  isMobile() {
-    const userAgent = navigator.userAgent.toLowerCase();
-    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
-  }
-  
-  trackMouseMovement() {
-    let animationFrame; // Declare animationFrame within the function
-    
-    document.addEventListener(this.isMobile() ? "touchmove" : "mousemove", (e) => {
-      const x = this.isMobile() ? e.touches[0].clientX : e.clientX;
-      const y = this.isMobile() ? e.touches[0].clientY : e.clientY;
-      
-      const squares = document.querySelectorAll(".square");
-      
-      // Cancel any pending animation frame
-      cancelAnimationFrame(animationFrame);
-      
-      animationFrame = requestAnimationFrame(() => {
-        squares.forEach((square) => {
-          const squareX = square.offsetLeft;
-          const squareY = square.offsetTop;
-          const squareWidth = square.offsetWidth;
-          const squareHeight = square.offsetHeight;
-          const squareXCenter = squareX + squareWidth / 2;
-          const squareYCenter = squareY + squareHeight / 2;
-          const distanceX = x - squareXCenter;
-          const distanceY = y - squareYCenter;
-          
-          // Calculate the distance from the mouse to the center of the square
-          const distance = Math.sqrt(
-            distanceX * distanceX + distanceY * distanceY
-          );
-          
-          // Scale the distance to a value between 0 and 1
-          const scale = Math.min(1, distance / 100);
-          
-          // Apply the scale to the transform property of the square
-          square.style.transform = `scale(${1 - scale})`;
-          
-          // Change the background color based on the distance
-          const color = `rgba(255, 255, 255, ${1 - scale})`;
-          square.style.borderColor = color;
-          square.style.borderWidth = `${scale * 10}px`;
-          square.style.borderStyle = "solid";
-          
-          if (scale > 0.8) {
-            square.style.opacity = 0;
-          } else {
-            square.style.opacity = 1;
-          }
+
+    // Methods
+    /**
+     * Handle open menu button
+     */
+    menuBtn() {
+        Dataclass.menuBtn.addEventListener("click", () => {
+            const clicked = Dataclass.menuBtn.classList.toggle("c");
+            if (clicked) {
+                this.openAsideMenu();
+            } else {
+                this.closeAsideMenu();
+            }
         });
-      });
-    });
-    
-    document.addEventListener(this.isMobile() ? "touchend" : "mouseleave", () => {
-      cancelAnimationFrame(animationFrame);
-      const squares = document.querySelectorAll(".square");
-      squares.forEach((square) => {
-        square.style.transform = "scale(1)";
-        square.style.borderColor = "transparent";
-        square.style.borderWidth = "0px";
-        square.style.borderStyle = "none";
-        square.style.opacity = 1;
-      });
-    });
-  }
-  
-  fields() {
-    let subjects = ["Data science", "Web development", "Mobile development"]
-    var idx = 0;
-    const subject = document.getElementById("subject");
-    // subject.textContent = subjects[idx];
-    
-    function updateText() {
-      if (idx >= subjects.length) {
-        idx = 0;
-      }
-      subject.textContent = subjects[idx];
-      subject.dataset.subject = subjects[idx];
-      idx++
     }
-    
-    subject.onanimationiteration = updateText;
-    
-  }
+
+    menuItems() {
+        const lists = document.querySelectorAll(".aside-contents ul li p")
+
+        lists.forEach(li => {
+            li.addEventListener("click", () => {
+                this.menuItem(li.dataset.sectionId)
+            })
+        })
+    }
+
+    menuItem(sectionId) {
+        const clicked = Dataclass.menuBtn.classList.toggle("c");
+        if (clicked) {
+            this.openAsideMenu();
+        } else {
+            this.closeAsideMenu();
+            this.menuItmHyperlink(sectionId)
+        }
+    }
+
+    menuItmHyperlink(sectionId) {
+        document.querySelector(sectionId).scrollIntoView({
+            behavior: 'smooth'
+        })
+    }
+
+
+    background() {
+        for (let i = 0; i < 180; i++) {
+            // Create the div container with square class
+            const div = document.createElement("div");
+            div.classList.add("square");
+            div.classList.add("square-" + i);
+
+            // Append the div element to design container
+            Dataclass.designContainer.appendChild(div);
+        }
+    }
+
+    isMobile() {
+        const userAgent = navigator.userAgent.toLowerCase();
+        return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+    }
+
+    trackMouseMovement() {
+        let animationFrame; // Declare animationFrame within the function
+
+        document.addEventListener(this.isMobile() ? "touchmove" : "mousemove", (e) => {
+            const x = this.isMobile() ? e.touches[0].clientX : e.clientX;
+            const y = this.isMobile() ? e.touches[0].clientY : e.clientY;
+
+            const squares = document.querySelectorAll(".square");
+
+            // Cancel any pending animation frame
+            cancelAnimationFrame(animationFrame);
+
+            animationFrame = requestAnimationFrame(() => {
+                squares.forEach((square) => {
+                    const squareX = square.offsetLeft;
+                    const squareY = square.offsetTop;
+                    const squareWidth = square.offsetWidth;
+                    const squareHeight = square.offsetHeight;
+                    const squareXCenter = squareX + squareWidth / 2;
+                    const squareYCenter = squareY + squareHeight / 2;
+                    const distanceX = x - squareXCenter;
+                    const distanceY = y - squareYCenter;
+
+                    // Calculate the distance from the mouse to the center of the square
+                    const distance = Math.sqrt(
+                        distanceX * distanceX + distanceY * distanceY
+                    );
+
+                    // Scale the distance to a value between 0 and 1
+                    const scale = Math.min(1, distance / 100);
+
+                    // Apply the scale to the transform property of the square
+                    square.style.transform = `scale(${1 - scale})`;
+
+                    // Change the background color based on the distance
+                    const color = `rgba(255, 255, 255, ${1 - scale})`;
+                    square.style.borderColor = color;
+                    square.style.borderWidth = `${scale * 10}px`;
+                    square.style.borderStyle = "solid";
+
+                    if (scale > 0.8) {
+                        square.style.opacity = 0;
+                    } else {
+                        square.style.opacity = 1;
+                    }
+                });
+            });
+        });
+
+        document.addEventListener(this.isMobile() ? "touchend" : "mouseleave", () => {
+            cancelAnimationFrame(animationFrame);
+            const squares = document.querySelectorAll(".square");
+            squares.forEach((square) => {
+                square.style.transform = "scale(1)";
+                square.style.borderColor = "transparent";
+                square.style.borderWidth = "0px";
+                square.style.borderStyle = "none";
+                square.style.opacity = 1;
+            });
+        });
+    }
+
+    fields() {
+        let subjects = ["Data science", "Web development", "Mobile development"]
+        var idx = 0;
+        const subject = document.getElementById("subject");
+
+        // subject.textContent = subjects[idx];
+
+        function updateText() {
+            if (idx >= subjects.length) {
+                idx = 0;
+            }
+            subject.textContent = subjects[idx];
+            subject.dataset.subject = subjects[idx];
+            idx++
+        }
+
+        subject.onanimationiteration = updateText;
+
+    }
 }
--- scripts/intersection.js
+++ scripts/intersection.js
@@ -1,140 +1,134 @@
+// The LORD GOD is holy
+
+
 let text
 
 const innerObserverCallback = (entries, observer) => {
-  entries.forEach((entry) => {
-    /*entry.target.classList.add("inner-pro-container-hide")*/
-    
-    if (entry.isIntersecting) {
-      if (entry.intersectionRatio >= 0.4) {
-        entry.target.classList.add("inner-pro-container-display")
-      }
-    }
-    
-    else {
-      entry.target.classList.remove("inner-pro-container-display")
-      entry.target.classList.add("inner-pro-container-hide")
-    }
-  })
-}
+    entries.forEach((entry) => {
+        /*entry.target.classList.add("inner-pro-container-hide")*/
 
-function proContents() {
-  const outerOption = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1
-  }
-  const outerObserver = new IntersectionObserver(() => {
-    const options = {
-      root: null,
-      rootMargin: "20px",
-      threshold: 0.4,
-    };
-    
-    const targets = document.querySelectorAll(".inner-pro-container, .pro-title-container")
-    
-    const observer = new IntersectionObserver(innerObserverCallback, options);
-    
-    targets.forEach(it => observer.observe(it))
-  }, outerOption)
-  
-  outerObserver.observe(document.querySelector("#project-section"))
-}
-
-function proIntersection(entry, seperator, subTile) {
-  if (entry.isIntersecting) {
-    text = "Project"
-    const proH2 = document.querySelector(".pro-title-container h2")
-    proH2.classList.remove("show")
-    proH2.classList.add("hide")
-    
-    seperator.classList.remove("hide")
-    seperator.classList.add("show")
-    
-    subTile.classList.remove("hide")
-    subTile.classList.add("show")
-  }
-  else {
-    const proH2 = document.querySelector(".pro-title-container h2")
-    proH2.classList.remove("hide")
-    proH2.classList.add("show")
-    
-    seperator.classList.remove("show")
-    seperator.classList.add("hide")
-    
-    subTile.classList.remove("show")
-    subTile.classList.add("hide")
-  }
-}
-
-export function arrowIntersection() {
-  const callback = (entries, observer) => {
-    
-    // Fetch the arrow-contain class
-    const arrows = document.querySelectorAll('.arrow-container .arrow-inner-container');
-    
-    entries.forEach(entry => {
-        arrows.forEach(arrow => {
-          if (entry.isIntersecting) {
-            arrow.classList.remove('hide-arrow')
-          } else {
-            arrow.classList.add('hide-arrow')
-            
-          }
-        })
+        if (entry.isIntersecting) {
+            if (entry.intersectionRatio >= 0.4) {
+                entry.target.classList.add("inner-pro-container-display")
+            }
+        } else {
+            entry.target.classList.remove("inner-pro-container-display")
+            entry.target.classList.add("inner-pro-container-hide")
+        }
     })
 }
 
-const options = {
-  root: null,
-  rootMargin: "50px 0px",
-  threshold: 0.9
+function proContents() {
+    const outerOption = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1
+    }
+    const outerObserver = new IntersectionObserver(() => {
+        const options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 0.4,
+        };
+
+        const targets = document.querySelectorAll(".inner-pro-container, .pro-title-container")
+
+        const observer = new IntersectionObserver(innerObserverCallback, options);
+
+        targets.forEach(it => observer.observe(it))
+    }, outerOption)
+
+    outerObserver.observe(document.querySelector("#project-section"))
 }
 
-const observer = new IntersectionObserver(callback, options);
+function proIntersection(entry, subTile) {
+    if (entry.isIntersecting) {
+        text = "project"
+        const proH2 = document.querySelector(".pro-title-container h2")
+        proH2.classList.remove("show")
+        proH2.classList.add("hide")
 
-const target = document.querySelector("#intro-section");
+        subTile.classList.remove("hide")
+        subTile.classList.add("show")
+        console.log("Hi")
+    } else {
+        const proH2 = document.querySelector(".pro-title-container h2")
+        proH2.classList.remove("hide")
+        proH2.classList.add("show")
 
-observer.observe(target)
+        subTile.classList.remove("show")
+        subTile.classList.add("hide")
+        document.querySelector(".header-title-container").style.height = "fit-content"
+    }
+}
+
+export function arrowIntersection() {
+    const callback = (entries, observer) => {
+
+        // Fetch the arrow-contain class
+        const arrows = document.querySelectorAll('.arrow-container .arrow-inner-container');
+
+        entries.forEach(entry => {
+            arrows.forEach(arrow => {
+                if (entry.isIntersecting) {
+                    arrow.classList.remove('hide-arrow')
+                } else {
+                    arrow.classList.add('hide-arrow')
+
+                }
+            })
+        })
+    }
+
+    const options = {
+        root: null,
+        rootMargin: "50px 0px",
+        threshold: 0.9
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+
+    const target = document.querySelector("#intro-section");
+
+    observer.observe(target)
 }
 
 export function intersection() {
-  const callback = (entries, observer) => {
-    const seperator = document.getElementById("title-separator");
-    const subTile = document.getElementById("sub-title");
-    
-    let proTitle
-    
-    entries.forEach((entry) => {
-      // Each entry describes an intersection change for one observed
-      // target element:
-      //   entry.boundingClientRect
-      //   entry.intersectionRatio
-      //   entry.intersectionRect
-      //   entry.isIntersecting
-      //   entry.rootBounds
-      //   entry.target
-      //   entry.time
-      
-      proIntersection(
-        entry,
-        seperator,
-        subTile
-      )
-    });
-    subTile.textContent = text
-  };
-  
-  const options = {
-    root: null,
-    rootMargin: "20px",
-    threshold: 1.0,
-  };
-  
-  const observer = new IntersectionObserver(callback, options);
-  
-  const target = document.querySelector("#project-section");
-  
-  observer.observe(target)
-  
-  proContents()
+    const callback = (entries, observer) => {
+        const subTile = document.getElementById("sub-title");
+
+        let proTitle
+
+        entries.forEach((entry) => {
+            // Each entry describes an intersection change for one observed
+            // target element:
+            //   entry.boundingClientRect
+            //   entry.intersectionRatio
+            //   entry.intersectionRect
+            //   entry.isIntersecting
+            //   entry.rootBounds
+            //   entry.target
+            //   entry.time
+
+            proIntersection(
+                entry,
+                subTile
+            )
+        });
+        subTile.textContent = text
+    };
+
+    const options = {
+        root: null,
+        rootMargin: "20px",
+        threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    const target = document.querySelector("#project-section");
+
+    observer.observe(target)
+
+    proContents()
 }
